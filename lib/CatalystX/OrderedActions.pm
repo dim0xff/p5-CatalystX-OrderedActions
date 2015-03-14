f4553cc59a15434a87740268d441afa6ff464932
Dmitry Latin <dim0xff@gmail.com>
2015-03-15 01:10:44 +0300

    First commit

---
+++ lib/CatalystX/OrderedActions.pm
@@ -0,0 +1,223 @@
+package CatalystX::OrderedActions;
+
+# ABSTRACT: role for L<Catalyst> controller to "correct" actions order
+
+use Moose::Role;
+use namespace::autoclean;
+
+use Carp;
+use Scalar::Util 'looks_like_number';
+use List::MoreUtils 'uniq';
+
+use Data::Dumper;
+
+after setup_actions => sub {
+    my $self = shift;
+
+    my $ns = ref $self;
+    for my $dt ( @{ $self->dispatcher->dispatch_types } ) {
+        my $type = ref $dt;
+
+        $type =~ s/^Catalyst::DispatchType:://;
+
+        if ( $type eq 'Path' ) {
+            $self->log->debug("Reorder Path actions...") if $self->debug;
+
+#            warn Dumper($dt);
+
+            for my $actions ( values %{ $dt->_paths } ) {
+                croak "Not array" unless ref $actions eq 'ARRAY';
+
+                $actions
+                    = [ sort { $self->compare_actions( $a, $b ) } @{$actions} ];
+            }
+
+#            warn Dumper($dt);
+        }
+        elsif ( $type eq 'Chained' ) {
+            $self->log->debug("Reorder Chained actions...") if $self->debug;
+
+#            warn Dumper( $dt->_children_of );
+
+            for my $child ( values %{ $dt->_children_of } ) {
+                croak "Not hash" unless ref $child eq 'HASH';
+
+                for my $actions ( values %{$child} ) {
+                    croak "Not array" unless ref $actions eq 'ARRAY';
+
+                    #<<<
+                    my @zero_actions =
+                        sort { $self->compare_actions( $a, $b ) }
+                        grep {
+                            0 == ( ( $_->attributes->{Args} || [] )->[0] // -1 )
+                        } @{$actions};
+
+                    my @undef_actions =
+                        sort { $self->compare_actions( $a, $b ) }
+                        grep {
+                            not defined( ( $_->attributes->{Args} || [] )->[0] )
+                        } reverse @{$actions};
+
+                    my @args_actions =
+                        sort { $self->compare_actions( $a, $b ) }
+                        grep {
+                            0 < ( ( $_->attributes->{Args} || [] )->[0] // -1 )
+                        } reverse @{$actions};
+
+                    $actions = [
+                        reverse(@args_actions),
+                        reverse(@zero_actions),
+                        reverse(@undef_actions),
+                    ];
+                }
+            }
+
+#            warn Dumper( $dt->_children_of );
+        }
+        elsif ( $self->debug ) {
+            $self->log->debug("Don't know how to reorder $type actions...");
+        }
+    }
+};
+
+
+# Compare rules to future compare sort order.
+# HASH with rules:
+# (
+# RULE => DEFINITION
+# )
+# RULE := string
+# DEFINITION:= -1 | 0 | 1 | coderef
+#
+# Default rule (no sorting): * => 0
+#
+# When DEFINITION is integer, then RULE is being used
+# as action attribute key to get values:
+# $self->attributes->{RULE} * DEFINITION
+#
+# When DEFINITION is coderef, then rule value is
+# result of calling DEFINITION->($self)
+
+sub _compare_rules {
+    return (
+        '*'  => 0,
+        Args => sub {
+            my $action = shift;
+
+            my ($val) = @{ $action->attributes->{Args} || [] };
+
+            return looks_like_number($val) ? $val : ~0;
+        },
+        Scheme   => -1,
+        Method   => -1,
+        Consumes => -1,
+
+    );
+}
+
+# Rules keys. Order of keys is equal to compare order checks
+sub _action_compare_keys {
+    my ( $self, $action ) = @_;
+    my @known = ( 'Args', 'Scheme', 'Method', 'Consumes' );
+
+    my @available;
+    for my $key (@known) {
+        push( @available, $key ) if exists $action->attributes->{$key};
+    }
+
+    return @available;
+}
+
+# rule definition for specified rule
+sub _compare_rule {
+    my ( $self, $attr ) = @_;
+
+    my %rules = $self->_compare_rules;
+
+    return ( $rules{$attr} // $rules{'*'} );
+}
+
+# rule value for specified rule
+sub _action_compare_value {
+    my ( $self, $action, $attr ) = @_;
+
+    my $rule = $self->_compare_rule($attr);
+
+    if ( ref $rule eq 'CODE' ) {
+        return $rule->($action);
+    }
+    else {
+        return 0 unless exists $action->attributes->{$attr};
+        return $rule * @{ $action->attributes->{$attr} || [] };
+    }
+}
+
+sub compare_actions {
+    my ( $self, $a1, $a2 ) = @_;
+
+    my %cmp = (
+        a1 => {},
+        a2 => {},
+    );
+
+    my @a1keys = $self->_action_compare_keys($a1);
+    my @a2keys = $self->_action_compare_keys($a2);
+
+    for my $attr ( uniq( @a1keys, @a2keys ) ) {
+        $cmp{a1}{$attr} = $self->_action_compare_value( $a1, $attr ) * @a1keys;
+        $cmp{a2}{$attr} = $self->_action_compare_value( $a2, $attr ) * @a2keys;
+    }
+
+    my $cmp = 0;
+    $cmp ||= $cmp{a1}{$_} <=> $cmp{a2}{$_} for uniq( @a1keys, @a2keys );
+
+    return $cmp;
+}
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+    package MyApp;
+    use Catalyst qw(+CatalystX::OrderedActions);
+
+
+    package MyApp::Controller::Resource;
+
+    use base qw(Catalyst::Controller);
+    use Moose;
+    with 'CatalystX::TraitFor::Controller::OrderedActions';
+
+    sub show    : GET     Path('/res') Args(1) { ... }
+    sub modify  :         Path('/res') Args(1) { ... }
+    sub options : OPTIONS Path('/res') Args(1) { ... }
+
+=head1 DESCRIPTION
+
+This role tries to add actions matching order based on priorities into
+L<Path|Catalyst::DispatchType::Path>
+and L<Chained|Catalyst::DispatchType::Chained> dispatch types.
+
+=head1 MATCHING PRIORITY
+
+Path and Chained actions should respect addition matching logic.
+
+Dispatcher should match path to action with some priority (matching order).
+
+Each matching order position should depends on Args and fact of existing addition
+matching (in group, like SQL ORDER BY by several fields).
+
+=head2 Path
+
+...
+
+=head2 Chained
+
+...
+
+=head1 SEE ALSO
+
+For discussion you could refer to github pull request
+L<#87|https://github.com/perl-catalyst/catalyst-runtime/pull/87>.
---
+++ t/001-basic.t
@@ -0,0 +1,145 @@
+use strict;
+use warnings;
+
+use Test::Most;
+use lib 't/lib';
+
+use HTTP::Request::Common qw(GET POST DELETE PUT);
+
+package MyApp::Controller::Root {
+    use Moose;
+    use MooseX::MethodAttributes;
+
+    extends 'Catalyst::Controller';
+
+    __PACKAGE__->config( namespace => '' );
+
+    sub index : Index {
+        $_[1]->res->body('index');
+    }
+
+
+    # Path
+    sub any : Path Args {
+        $_[1]->res->body('any');
+    }
+
+    sub loc : Local Args(0) {
+        $_[1]->res->body('loc');
+    }
+
+    sub path_get_any : GET Path('loc') Args {
+        $_[1]->res->body('GET path any');
+    }
+
+    sub path_get_zero : GET Path('loc') Args(0) {
+        $_[1]->res->body('GET path zero');
+    }
+
+    sub path_get_one : GET Path('loc') Args(1) {
+        $_[1]->res->body('GET path one');
+    }
+
+    sub another_loc : Path('loc') Args(0) {
+        $_[1]->res->body('another loc');
+    }
+
+    sub another_path_get_any : GET Path('loc') Args {
+        $_[1]->res->body('GET another path any');
+    }
+
+    sub another_any : Path Args {
+        $_[1]->res->body('another any');
+    }
+
+
+    # Chained
+    sub base : Chained('/') PathPart('chain') CaptureArgs(0) { }
+
+    sub chained_any_get : GET Chained('base') PathPart('') Args {
+        $_[1]->res->body('chained any get');
+    }
+
+    sub chained_any1 : Chained('base') PathPart('') Args {
+        $_[1]->res->body('chained any first');
+    }
+
+    sub chained_any2 : Chained('base') PathPart('') Args {
+        $_[1]->res->body('chained any second');
+    }
+
+    sub chained_any_post : POST Chained('base') PathPart('') Args {
+        $_[1]->res->body('chained any post');
+    }
+
+
+    sub chained_zero_get : GET Chained('base') PathPart('') Args(0) {
+        $_[1]->res->body('chained zero get');
+    }
+
+    sub chained_zero1 : Chained('base') PathPart('') Args(0) {
+        $_[1]->res->body('chained zero first');
+    }
+
+    sub chained_zero2 : Chained('base') PathPart('') Args(0) {
+        $_[1]->res->body('chained zero second');
+    }
+
+    sub chained_zero_post : POST Chained('base') PathPart('') Args(0) {
+        $_[1]->res->body('chained zero post');
+    }
+
+
+    sub chained_one_get : GET Chained('base') PathPart('') Args(1) {
+        $_[1]->res->body('chained one get');
+    }
+
+    sub chained_one1 : Chained('base') PathPart('') Args(1) {
+        $_[1]->res->body('chained one first');
+    }
+
+    sub chained_one2 : Chained('base') PathPart('') Args(1) {
+        $_[1]->res->body('chained one second');
+    }
+
+    sub chained_one_post : POST Chained('base') PathPart('') Args(1) {
+        $_[1]->res->body('chained one post');
+    }
+};
+
+
+package MyApp {
+    use Catalyst qw(+CatalystX::OrderedActions);
+
+    #MyApp->config(show_internal_actions => 1);
+    MyApp->setup();
+};
+use Catalyst::Test 'MyApp';
+
+
+subtest 'Path' => sub {
+    is( request( GET '/' )->content,         'index' );
+    is( request( GET '/loc/1' )->content,    'GET path one' );
+    is( request( GET '/loc/1/2' )->content,  'GET another path any' );
+    is( request( POST '/loc/1/2' )->content, 'another any' );
+    is( request( POST '/loc/1' )->content,   'another any' );
+    is( request( GET '/loc' )->content,      'GET path zero' );
+    is( request( POST '/loc' )->content,     'another loc' );
+};
+
+
+subtest 'Chained' => sub {
+    is( request( GET '/chain/1/2' )->content,  'chained any get' );
+    is( request( POST '/chain/1/2' )->content, 'chained any post' );
+    is( request( PUT '/chain/1/2' )->content,  'chained any second' );
+
+    is( request( GET '/chain/1' )->content,  'chained one get' );
+    is( request( POST '/chain/1' )->content, 'chained one post' );
+    is( request( PUT '/chain/1' )->content,  'chained one second' );
+
+    is( request( GET '/chain' )->content,  'chained zero get' );
+    is( request( POST '/chain' )->content, 'chained zero post' );
+    is( request( PUT '/chain' )->content,  'chained zero second' );
+};
+
+done_testing;
